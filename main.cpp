db48e071c21e5d0255f07a7f4b1c148315174a04
igorpuszman <igibabes@gmail.com>
2020-05-29 08:44:44 +0200

    ddd

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+srand( time( NULL ) );
+rand () %10;
+int main(int argc, char** argv) {
+ ofstream wyjscie;
+ wyjscie.open("out.json");
+ cout << Wypisz Liczbe Losowa (10);
+ 
+ wyjscie<<"Wypisz Liczbe JSON";
+ wejscie.close();
+ wyjscie.close();
+	return 0;
+}
